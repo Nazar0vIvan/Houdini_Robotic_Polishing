1bebeaa8c8f9a3e618c54fb19a91bbf0a2719891
Boroda <vano888888@gmail.com>
2021-11-10 17:15:59 +0300

    initial commit

---
+++ libs/ik_KR10_R1420.py
@@ -0,0 +1,6 @@
+import numpy as np
+from math import * 
+
+d = np.array([223.0, -101.5, 101.5, -660.0, 0.0, -80])
+a = np.array ([150.0, -610.0, -20.0, 0.0, 0.0, 0.0])
+alpha = np.array ([pi/2, 0, -pi/2, pi/2, -pi/2, pi])
