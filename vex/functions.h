2c14e223ae51dfd3f15fc52514a195abbbfb38fe
BormachBoroda <vano888888@gmail.com>
2023-08-24 18:19:53 +0300

    chore: not essential changes

--- vex/functions.h
+++ vex/functions.h
@@ -1,3 +1,25 @@
+function vector4 plane(vector norm; vector pt) {
+    float A = norm[0];
+    float B = norm[1];
+    float C = norm[2];
+    float D = -A*pt[0]-B*pt[1]-C*pt[2];
+    
+    return set(A,B,C,D);
+}
+
+function vector4 plane(vector pt1; vector pt2; vector pt3) {
+    float x1 = pt1[0]; float y1 = pt1[1]; float z1 = pt1[2];
+		float x2 = pt2[0]; float y2 = pt2[1]; float z2 = pt2[2];
+    float x3 = pt3[0]; float y3 = pt3[1]; float z3 = pt3[2];
+    
+    float A = determinant(set(1,y2,z2,1,y1,z1,1,y2,z2));
+    float B = determinant(set(x2,1,z2,x1,1,z1,x3,1,z3));
+    float C = determinant(set(x2,y2,1,x1,y1,1,x3,y3,1));
+    float D = -determinant(set(x2,y2,z2,x1,y1,z1,x3,y3,z3));
+    
+    return set(A,B,C,D);
+}
+
 function vector pts2vector(vector pt1; vector pt2) {
     // v = {v1, v1, v3} - направляющий вектор прямой
     float vx = pt2[0] - pt1[0];
