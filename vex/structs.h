ac6df5469dec26718ea006ecbf01f51477bef2da
BormachBoroda <vano888888@gmail.com>
2023-09-22 15:47:35 +0300

    chore: not essential changes

--- vex/structs.h
+++ vex/structs.h
@@ -9,7 +9,19 @@ struct EdgeCirc {
 }
 
 struct RuledSurf {
-	vector p1, p2, p3, p4;
+	vector a1, a2, b1, b2;
+	
+	function vector val(float u; float v) {
+		return (1-u)*(1-v)*this.a1 + u*(1-v)*this.a2 + (1-u)*v*this.b1 + u*v*this.b2;
+	}
+	
+	function vector du(float v) {
+		return (this.a1 - this.a2)*(v-1) - (this.b1 - this.b2)*v;
+	}
+	
+	function vector dv(float u) {
+		return (this.a1 - this.b1)*(u-1) - (this.a2 - this.b2)*u;
+	}
 }
 
 struct Cone {
