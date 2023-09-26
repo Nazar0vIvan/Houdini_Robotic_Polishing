5f38321dd96b874ed7d9437711071c29d565ce3a
Nazar0vIvan <vano888888@gmail.com>
2023-09-27 02:49:53 +0300

    chore: not essential changes

--- vex/functions.h
+++ vex/functions.h
@@ -1,28 +1,35 @@
-function vector4 plane(vector norm; vector pt) {
-    float A = norm[0];
-    float B = norm[1];
-    float C = norm[2];
-    float D = -A*pt[0]-B*pt[1]-C*pt[2];
-    
-    return set(A,B,C,D);
+function float[] plane(vector norm; vector pt) {
+		float A = norm[0],
+				  B = norm[1],
+			    C = norm[2],
+					D = -A*pt[0]-B*pt[1]-C*pt[2];
+    return array(A,B,C,D);
 }
 
-function vector4 plane(vector pt1; vector pt2; vector pt3) {
-    float x1 = pt1[0]; float y1 = pt1[1]; float z1 = pt1[2];
-		float x2 = pt2[0]; float y2 = pt2[1]; float z2 = pt2[2];
-    float x3 = pt3[0]; float y3 = pt3[1]; float z3 = pt3[2];
-    
-    float A = determinant(set(1,y2,z2,1,y1,z1,1,y3,z3));
-    float B = determinant(set(x2,1,z2,x1,1,z1,x3,1,z3));
-    float C = determinant(set(x2,y2,1,x1,y1,1,x3,y3,1));
-    float D = -determinant(set(x2,y2,z2,x1,y1,z1,x3,y3,z3));
-    
-    return set(A,B,C,D);
+function float[] plane(vector pt1; vector pt2; vector pt3) {
+    float x1 = pt1[0],
+					y1 = pt1[1],
+					z1 = pt1[2],
+					x2 = pt2[0],
+					y2 = pt2[1],
+					z2 = pt2[2],
+					x3 = pt3[0],
+					y3 = pt3[1],
+					z3 = pt3[2];
+    float A = determinant(set(1,y2,z2,1,y1,z1,1,y3,z3)),
+					B = determinant(set(x2,1,z2,x1,1,z1,x3,1,z3)),
+					C = determinant(set(x2,y2,1,x1,y1,1,x3,y3,1)),
+					D = -determinant(set(x2,y2,z2,x1,y1,z1,x3,y3,z3));
+    return array(A,B,C,D);
 }
 
 function vector4 circ(vector pt1; vector pt2; vector pt3){
-    float x1 = pt1[0]; float x2 = pt2[0]; float x3 = pt3[0];
-    float y1 = pt1[1]; float y2 = pt2[1]; float y3 = pt3[1];
+    float x1 = pt1[0],
+				  x2 = pt2[0], 
+					x3 = pt3[0];
+    float y1 = pt1[1],
+					y2 = pt2[1], 
+					y3 = pt3[1];
     float xc = -0.5*(y1*(x2*x2+y2*y2-x3*x3-y3*y3)+y2*(x3*x3+y3*y3-x1*x1-y1*y1)+y3*(x1*x1+y1*y1-x2*x2-y2*y2))/(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
     float yc = 0.5*(x1*(x2*x2+y2*y2-x3*x3-y3*y3)+x2*(x3*x3+y3*y3-x1*x1-y1*y1)+x3*(x1*x1+y1*y1-x2*x2-y2*y2))/(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
     float r = sqrt((x1-xc)*(x1-xc) + (y1-yc)*(y1-yc));
@@ -47,7 +54,9 @@ function vector poly(vector pt1; vector pt2; vector pt3) {
 }
 
 function vector2 polynorm(vector poly; float x0) {
-    float a = poly[0]; float b = poly[1]; float c = poly[2];
+    float a = poly[0],
+					b = poly[1],
+					c = poly[2];
     return set(-1/(2*a*x0+b), a*x0*x0+b*x0+c + x0/(2*a*x0+b));
 }
 
@@ -59,36 +68,6 @@ function float polyval(float a; float b; float c; float x0) {
     return a*pow(x0,2)+b*x0+c;
 }
 
-/*
-function vector4 lsqr(int ptnums[]) {
-	vector pt;
-	float X2 = 0.; float XY = 0.; float X = 0.;
-	float Y2 = 0.; float Y = 0.;
-	float XZ = 0.; float YZ = 0.; float Z = 0.;
-	foreach(int ptnum; ptnums) {
-			pt = point(0, "P", ptnum);
-			
-			X2 += pt[0]*pt[0]; Y2 += pt[1]*pt[1];
-			XY += pt[0]*pt[1];
-			X  += pt[0]; Y  += pt[1];
-			
-			XZ += pt[0]*pt[2];
-			YZ += pt[1]*pt[2];
-			Z  += pt[2];   
-	}
-	matrix3 U = set(X2, XY, X, XY, Y2, Y, X, Y, len(ptnums));
-	vector V = set(XZ, YZ, Z);
-	vector P = invert(U)*V;
-
-	float C = 1/sqrt(P[0]*P[0]+P[1]*P[1]+1);
-	float A = -P[0]*C;
-	float B = -P[1]*C;
-	float D = -P[2]*C;
-	
-	return set(A,B,C,D);
-}
-*/
-
 function int[] linspace(int x1; int x2; int is_end) {
     int res[];
     int k = x2 > x1 ? 1 : -1;
@@ -99,27 +78,6 @@ function int[] linspace(int x1; int x2; int is_end) {
 
 function void removepoints(int geohandle; int point_numbers[]) {
     foreach(int ptnum; point_numbers) removepoint(geohandle, ptnum);
-}
-
-/*
-function matrix3 invMatrix(matrix3 m) {
-    float m00 = getcomp(m, 0, 0); float m01 = getcomp(m, 0, 1); float m02 = getcomp(m, 0, 2);
-    float m10 = getcomp(m, 1, 0); float m11 = getcomp(m, 1, 1); float m12 = getcomp(m, 1, 2);
-    float m20 = getcomp(m, 2, 0); float m21 = getcomp(m, 2, 1); float m22 = getcomp(m, 2, 2);
-    
-    float invdet = 1/determinant(m);
-    
-    float im00 =  (m11*m22-m21*m12)*invdet;
-    float im10 = -(m01*m22-m02*m21)*invdet;
-    float im20 =  (m01*m12-m02*m11)*invdet;
-    float im01 = -(m10*m22-m12*m20)*invdet;
-    float im11 =  (m00*m22-m02*m20)*invdet;
-    float im21 = -(m00*m12-m10*m02)*invdet;
-    float im02 =  (m10*m21-m20*m11)*invdet;
-    float im12 = -(m00*m21-m20*m01)*invdet;
-    float im22 =  (m00*m11-m10*m01)*invdet;
-    
-    return set(im00, im10, im20, im01, im11, im21, im02, im12, im22);
 }
 
 function void printDict(dict dic) {
@@ -137,14 +95,16 @@ function void printMatrix(matrix3 m) {
     printf("\n");
     }
 }
-*/
 
 // intersection of the line (points u1, u2) and cone, defined by generator (line) kg*x+bg
 function vector intersec_cone_line(vector u1; vector u2; float kg; float bg) {
     vector vec = u2 - u1;
-    float vx = vec[0]; float vy = vec[1]; float vz = vec[2];
-    float x1 = u1[0]; float y1 = u1[1]; float z1 = u1[2];
-
+    float vx = vec[0],
+			    vy = vec[1],
+					vz = vec[2];
+    float x1 = u1[0],
+				  y1 = u1[1],
+					z1 = u1[2];
     float a = pow(vy/kg,2) - pow(vx,2) + pow(vz/kg,2);
     float b = 2*vy*y1/(pow(kg,2)) - 2*vx*(x1+bg/kg) + 2*vz*z1/(pow(kg,2));
     float c = pow(y1/kg,2) - pow(x1+bg/kg,2) + pow(z1/kg,2);
@@ -153,98 +113,4 @@ function vector intersec_cone_line(vector u1; vector u2; float kg; float bg) {
     solvequadratic(a,b,c,t1,t2);
     
     return set(x1 + vx*t1, y1 + vy*t1, z1 + vz*t1);
-}
-
-// EDGE GENERATION
-
-struct EdgeCirc {
-    float xc, yc, zc, R;
-		vector2 clipline1;
-		vector2 clipline2;
-}
-
-function EdgeCirc edgecirc_bytans(vector2 line1; vector2 line2; vector pt1; vector pt2) {  
-    float a1 = line1[0]; float b1 = line1[1];
-    float a2 = line2[0]; float b2 = line2[1];
-    float x1 = pt1[0]; float y1 = pt1[1];
-    float x2 = pt2[0]; float y2 = pt2[1];
-    float zc = pt1[2];
-    
-    float xc, yc, r, x2t, y2t, x1t, y1t, alpha1, alpha2;
-    vector2 clipline1, clipline2;
-    
-    // circ1 - circ created from the x1; circ2 - circ created from the x2
-    // check position of circ1 touch point (x2t) relative to x2
-    xc = (a2*x1*(1+a1*a1)*(a2-a1) + a1*((b2-b1)*(1+a1*a2) + sqrt((a1*a1+1)*(a2*a2+1))*(b1-b2+x1*(a1-a2))))/pow((a1-a2),2);
-    x2t = (b1-b2+xc/a2+(x1-xc)/a1+a1*x1)/(a2+1/a2);
-    if(abs(x2t) > abs(x2)) {
-        // continue create circ1
-        r = abs((x1 - xc + a1*a1*x1 + a1*b1 - a1*b2 - a1*a2*xc)/(a1*sqrt(a2*a2+1)));
-        yc = a1*x1+b1 - 1/a1*(xc-x1);
-        y2t = a2*x2t+b2;
-        clipline1 = linsolve(x1, xc, y1, yc);
-        clipline2 = linsolve(x2t, xc, y2t, yc); 
-    } else {
-        // start create circ2
-        xc = (a1*x2*(1+a2*a2)*(a1-a2) - a2*((b2-b1)*(1+a1*a2) + sqrt((a1*a1+1)*(a2*a2+1))*(b1-b2+x2*(a1-a2))))/pow((a1-a2),2);
-        r = abs((x2 - xc + a2*a2*x2 - a2*b1 + a2*b2 - a1*a2*xc)/(a2*sqrt(a1*a1+1)));
-        yc = a2*x2+b2 - 1/a2*(xc-x2);
-        x1t = (b2-b1+xc/a1+(x2-xc)/a2+a2*x2)/(a1+1/a1);
-        y1t = a1*x1t+b1;
-        clipline1 = abs(x1) > abs(x1t) ? linsolve(x1, xc, y1, yc) : linsolve(x1t, xc, y1t, yc);
-        clipline2 = linsolve(x2, xc, y2, yc); 
-    }
-    return EdgeCirc(xc, yc, zc, r, clipline1, clipline2);
-}
-
-
-function int[] addpoints_edgepf(EdgeCirc circ; int n; string edgetype) {
-    float k1 = circ.clipline1[0]; float k2 = circ.clipline2[0];
-    float z = circ.zc;
-    float ba; // begin angle
-    float ea; // end angle
-    if (abs(k1) < 0.0001) {
-        if (edgetype == "le") {
-            ba = PI/2;
-            ea = k2 > 0 ? PI/2+atan(k2) : 3*PI/2+atan(k2);  
-        }
-        else {
-            ba = k2 > 0 ? -PI+atan(k2) : atan(k2);
-            ea = k2 > 0 ? 3*PI/2-atan(k2) : PI/2-atan(k2);
-        }
-    }
-    else {
-        if(abs(k2) < 0.0001) {
-            if (edgetype == "le") {
-                ba = k1 > 0 ? atan(k1) : PI+atan(k1);
-                ea = k1 > 0 ? 3*PI/2-atan(k1) : PI/2-atan(k1);
-            }
-            else {
-                ba = -PI/2;
-                ea = k1 > 0 ? PI/2+atan(k1) : 3*PI/2+atan(k1);
-            }
-        }
-        else{ // k1 != 0 && k2 != 0
-            if(edgetype == "le") {
-                ba = k1 > 0 ? atan(k1) : PI + atan(k1);
-                ea = k1*k2 > 0 ? PI-atan(k1)+atan(k2) : k1 > 0 ? 2*PI-atan(k1)+atan(k2) : -atan(k1)+atan(k2);
-            }
-            else {
-                ba = k2 < 0 ? atan(k2) : -PI+atan(k2);
-                ea = k1*k2 > 0 ? PI+atan(k1)-atan(k2) : k1 < 0 ? 2*PI+atan(k1)-atan(k2) : atan(k1)-atan(k2);  
-            }
-        }
-    }
-    float step = ea/n;
-    float t = step;
-    int ptnums []; int ptnum; 
-    for(int i = 0; i < n-1; i++) {
-        float x = circ.xc + circ.R*cos(t + ba);
-        float y = circ.yc + circ.R*sin(t + ba);
-        ptnum = addpoint(0, set(x,y,z) );
-        setpointattrib(0, "blade_part", ptnum, edgetype);
-        push(ptnums, ptnum);
-        t += step;
-     }
-     return ptnums;
 }
--- vex/plane.h
+++ vex/plane.h
@@ -7,4 +7,19 @@ struct Plane {
 					cc = -this.coeffs[3]/this.coeffs[2];
 		return (pt[2] > aa*pt[0] + bb*pt[1] + cc) ? 1 : 0;
 	}
+	function vector intersec(vector pt1; vector pt2) {
+    float A = this.coeffs[0],
+          B = this.coeffs[1],
+          C = this.coeffs[2],
+          D = this.coeffs[3];
+    float x1 = pt1[0],
+          y1 = pt1[1],
+          z1 = pt1[2];
+		vector dir_vec = pt1 - pt2;
+    float vx = dir_vec[0], 
+          vy = dir_vec[1],
+          vz = dir_vec[2];
+    float t = -(A*x1+B*y1+C*z1+D)/(A*vx+B*vy+C*vz);
+    return set(x1 + vx*t, y1 + vy*t, z1 + vz*t);
+	}
 }
