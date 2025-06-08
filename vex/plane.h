f2525817404704f39e7ee7757dd1be1a467d6767
BormachBoroda <vano888888@gmail.com>
2025-06-09 02:52:33 +0300

    chore: not essential changes

--- vex/plane.h
+++ vex/plane.h
@@ -1,13 +1,17 @@
 struct Plane {
-	vector4 coeffs;
+  vector4 coeffs;
 	
-	function int is_above(vector pt) {
-		float aa = -this.coeffs[0]/this.coeffs[2],
-					bb = -this.coeffs[1]/this.coeffs[2],
-					cc = -this.coeffs[3]/this.coeffs[2];
-		return (pt[2] > aa*pt[0] + bb*pt[1] + cc) ? 1 : 0;
-	}
-	function vector intersec(vector pt1; vector pt2) {
+  function float distance(vector pt) {
+    return A*pt[0] + B*pt[1] + C*pt[2] + D;
+  }
+	
+  function int is_above(vector pt) {
+    float aa = -this.coeffs[0]/this.coeffs[2],
+          bb = -this.coeffs[1]/this.coeffs[2],
+          cc = -this.coeffs[3]/this.coeffs[2];
+          return (pt[2] > aa*pt[0] + bb*pt[1] + cc) ? 1 : 0;
+  }
+  function vector intersec(vector pt1; vector pt2) {
     float A = this.coeffs[0],
           B = this.coeffs[1],
           C = this.coeffs[2],
@@ -21,5 +25,5 @@ struct Plane {
           vz = dir_vec[2];
     float t = -(A*x1+B*y1+C*z1+D)/(A*vx+B*vy+C*vz);
     return set(x1 + vx*t, y1 + vy*t, z1 + vz*t);
-	}
+  }
 }
--- vex/test.h
+++ vex/test.h
@@ -1,4 +1,50 @@
-struct Element {
-    int id;
-		string name;
+// VEX implementation for Houdini
+function int[] findAvailableIndices(int num_points) {
+    int indices[];
+    for (int i = 0; i < num_points; i++) {
+        append(indices, i);
+    }
+    return indices;
+}
+
+function float planeDistance(vector p; float A, B, C, D) {
+    return A*p.x + B*p.y + C*p.z + D;
+}
+
+function int[][] findPointPairs(vector curves[][]; float A, B, C, D) {
+    int pairs[][];
+    int num_curves = len(curves);
+    if (num_curves < 2) return pairs;
+    
+    int num_points = len(curves[0]);
+    int available_indices[] = findAvailableIndices(num_points);
+    
+    for (int j = 0; j < num_curves - 1; j++) {
+        int i = 0;
+        while (i < len(available_indices)) {
+            int idx = available_indices[i];
+            vector p_below = curves[j][idx];
+            vector p_above = curves[j+1][idx];
+            
+            float dist_below = planeDistance(p_below, A, B, C, D);
+            float dist_above = planeDistance(p_above, A, B, C, D);
+            
+            if (dist_below < 0 && dist_above > 0) {
+                // Found a valid pair
+                int pair[] = array(idx, j, j+1); // Stores (point_idx, curve_below, curve_above)
+                append(pairs, pair);
+                
+                // Remove this index from future consideration
+                removeindex(available_indices, i);
+                
+                // Early exit if all points paired
+                if (len(pairs) == num_points) {
+                    return pairs;
+                }
+            } else {
+                i++;
+            }
+        }
+    }
+    return pairs;
 }
