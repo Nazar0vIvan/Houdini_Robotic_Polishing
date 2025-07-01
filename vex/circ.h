41342a8588a2efac0be7976e13f7010c9a40b365
BormachBoroda <vano888888@gmail.com>
2025-07-01 19:45:07 +0300

    chore: not essential changes

--- vex/circ.h
+++ vex/circ.h
@@ -3,13 +3,18 @@ struct Circle {
 		float radius;
 }
 
-int[] addCirc(Circle c; int n) {
-    int pts[];
-    for (int i = 0; i < n; i++) {
-        float angle = 2*PI*i/n;
-        vector pos = c.center + set(cos(angle), sin(angle), 0) * c.radius;
-        append(pts, addpoint(0, pos));
+function int[] addCirc(Circle circ; int n) {
+    int pt_nums[];
+    vector pc = circ.center;
+		float r = circ.radius;
+		vector pt;
+		float angle;
+		for (int i = 0; i < n; i++) {
+        angle = 2*PI*i/n;
+        pt = pc + set(cos(angle), sin(angle), 0) * r;
+        append(pt_nums, addpoint(0, pt));
     }
-    addprim(0, "polyline", pts);
-    return pts;
+    if (inOnlyPoints) addprim(0, "polyline", pts);
+    return pt_nums;
 }
+
